ea6302d1980b15eb7bd9038459c62d250991ce00
Vipul Sharma <vipulsharma@Vipuls-MacBook-Pro.local>
2025-05-26 12:58:19 +0530

    fourth commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -12,12 +12,13 @@ import { useNavigate } from 'react-router-dom';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+  axios.defaults.baseURL = 'https://documentmanagerbackend-2.onrender.com';
 
   // Set default Authorization header
   useEffect(() => {
